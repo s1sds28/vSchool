432b013f5eead98b03695fda61ba5fe107503efd
Steven Sparks <s1sds28@gmail.com>
2023-07-28 13:56:16 -0400

    Post level1 Mid-level Review

--- evaluation/sp40_codeChallenge/index.js
+++ evaluation/sp40_codeChallenge/index.js
@@ -2,32 +2,56 @@
 /**Create a constructor function called Book that takes in parameters for title, author, and year.
 * The constructor function should have a method called getDetails that returns a string with the
 * book's title, author, and year.  Instantiate two new Books using the below code to test your solution.
-*/
-class Book{
-    constructor(title, author, year) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    }
-    getDetails() {
-        let keys = Object.keys(this)
-        let values = Object.values(this)
-        let entries = ""
-        for(let i=0; i<Object.keys.length; i++){
-            entries += "Title: " + this.title + ","
-            entries += " Author: " + this.author + ","
-            entries += " Year: " + this.year
-        }
-        return entries
-    }
+// */
+
+//ES6
+// class Book{
+//     constructor(title, author, year) {
+//         this.title = title;
+//         this.author = author;
+//         this.year = year;
+//     }
+//     getDetails() {
+//         console.log("title: ", this.title)
+//         console.log("author :", this.author)
+
+//         // let keys = Object.keys(this)
+//         // let values = Object.values(this)
+//         // let e
+//         // let keys = Object.keys(this)
+//         // let values = Object.values(this)
+//         // let entries = ""
+//         // for(let i=0; i<Object.keys.length; i++){
+//         //     entries += "Title: " + this.title + ","
+//         //     entries += " Author: " + this.author + ","
+//         //     entries += " Year: " + this.year
+//         // }
+//         // return entries
+//     }
+// }
+
+
+//ES5
+function Book(title, author, year) {
+
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.getDetails = function(){
+        console.log("title: ", this.title)
+        console.log("author :", this.author)
 }
+}
+
 
 const book1 = new Book("TheGreat Gatsby", "F. Scott Fitzgerald", 1925);
 const book2 = new Book("To kill a Mockingbird", "Harper Lee", 1960);
 
+book1.getDetails()
 
-console.log(book1.getDetails())
-console.log(book2.getDetails())
+
+// console.log(book1.getDetails())
+// console.log(book2.getDetails())
 
 /**Write a function called **`removeDuplicates`** that takes an array of numbers as input and returns a new
 *array with duplicate values removed.  Use the below code to test your solution.
@@ -35,13 +59,18 @@ console.log(book2.getDetails())
 
 function removeDuplicates(listDuplicates) {
     let listNoDuplicates = []
-    for(let i = 0; i < listDuplicates.length; i++){
-        if(listNoDuplicates.includes(listDuplicates[i])){
-            continue
-        } else {
-            listNoDuplicates.push(listDuplicates[i])
+    for (let i = 0; i< listDuplicates.length; i++) {
+        if (!listNoDuplicates.includes(listDuplicates[i])) {
+    
+              listNoDuplicates.push(listDuplicates[i])
         }
+
     }
+        // if(listNoDuplicates.includes(listDuplicates[i])){
+        //     continue
+        // } else {
+        // }
+    
     return listNoDuplicates;
 }
 
