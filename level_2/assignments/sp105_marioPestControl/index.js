6068b5b85783d99a386cec271802071f6f046fca
Steven Sparks <s1sds28@gmail.com>
2023-08-18 14:16:17 -0400

    Complete Mario Pest Control

--- level_2/assignments/sp105_marioPestControl/index.js
+++ level_2/assignments/sp105_marioPestControl/index.js
@@ -1,14 +1,25 @@
 const form = document.form
-
 const goombas = form.goombas
 const bobomb = form.bobomb
 const cheepCheep = form.cheepCheep
 const submit = form.submit
+const resetbtn = document.getElementById("reset")
+
+//Total
 const coinsElement = document.getElementById("totalCoins")
 
+//Submit buttons finds total coins and adds it to coins element
 submit.addEventListener("click", function(event) {
     event.preventDefault()
     const coinsNumber = Number(goombas.value) * 5 + Number(bobomb.value) * 7 + Number(cheepCheep.value) * 11
     coinsElement.textContent = String(coinsNumber)
 });
 
+//Reset button resets all baddies and inputs
+resetbtn.addEventListener("click", function(event) {
+    event.preventDefault()
+    coinsElement.textContent = "";
+    goombas.value = "";
+    bobomb.value = "";
+    cheepCheep.value ="";
+});
