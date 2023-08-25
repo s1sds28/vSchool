ea39aab49ef091a0174247226aa7308837e863ab
Steven Sparks <s1sds28@gmail.com>
2023-08-25 13:16:18 -0400

    Complete mid level 2 review

--- level_2/assignments/sp105_marioPestControl/index.js
+++ level_2/assignments/sp105_marioPestControl/index.js
@@ -1,3 +1,4 @@
+// const form = document.getElementById("form")
 const form = document.form
 const goombas = form.goombas
 const bobomb = form.bobomb
@@ -5,17 +6,24 @@ const cheepCheep = form.cheepCheep
 const submit = form.submit
 const resetbtn = document.getElementById("reset")
 
+const submitbtn = document.getElementById("submit")
+
+console.log(form)
 //Total
 const coinsElement = document.getElementById("totalCoins")
 
+// document.addEventListener("DOMContentLoaded", function(){
+//     const form = document.getElementById("form")
+// })
 //Submit buttons finds total coins and adds it to coins element
-submit.addEventListener("click", function(event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault()
+    console.log("test")
     const coinsNumber = Number(goombas.value) * 5 + Number(bobomb.value) * 7 + Number(cheepCheep.value) * 11
     coinsElement.textContent = String(coinsNumber)
 });
 
-//Reset button resets all baddies and inputs
+// Reset button resets all baddies and inputs
 resetbtn.addEventListener("click", function(event) {
     event.preventDefault()
     coinsElement.textContent = "";
--- level_2/evaluation/midLevelAssessment/index.js
+++ level_2/evaluation/midLevelAssessment/index.js
@@ -9,14 +9,29 @@ const sortedNumbers = sortNumbers(numbers);
 console.log(sortedNumbers);  // Output: [1, 2, 4, 5, 7, 9]
 
 // Write a function that takes an array of strings and returns a new array where each string is converted to uppercase.
-function convertToUppercase(strings) {
-    return strings.map(function(string) {
-        return string.toUpperCase()
-    })
-}
+// function convertToUppercase(strings) {
+//     return strings.map(function(string) {
+//         return string.toUpperCase()
+//     })
+// }
 
-// const convertToUppercase = strings => strings.map(string => string.toUpperCase())
+const convertToUppercase = strings => strings.map(string => string[0].toUpperCase() + string.slice(1))
 // Example usage:
 const strings = ['hello', 'world', 'javascript'];
 const uppercaseStrings = convertToUppercase(strings);
 console.log(uppercaseStrings);  // Output: ['HELLO', 'WORLD', 'JAVASCRIPT']
+
+
+function findEvenNumbers(arr){
+    //use % to find even number and return an array containing them
+
+    // return arr.filter(function(current){
+    //     if(current % 2 === 0) {
+    //         return current
+    //     }
+    // })
+
+    return arr.filter(current => current % 2 === 0)
+}
+
+console.log(findEvenNumbers([1,2,3,4,5,6,7,8,9,10])) // [2,4,6,8,10]
