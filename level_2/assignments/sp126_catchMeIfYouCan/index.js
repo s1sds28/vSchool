9f95fc761eba866e7c5bb24d166ebf66623ac0f9
Steven Sparks <s1sds28@gmail.com>
2023-08-24 21:06:03 -0400

    Refactor sp126.5 Catch Me If You Can

--- level_2/assignments/sp126_catchMeIfYouCan/index.js
+++ level_2/assignments/sp126_catchMeIfYouCan/index.js
@@ -1,16 +1,17 @@
-//a) Write a function that returns the sum of two numbers. Throw an error if either argument is not of the data type number:
-
+// 1a) Write a function that returns the sum of two numbers. Throw an error if either argument is not of the data type number:
+console.log("\nquestion 1a)\n")
 const sum = (x, y) => {
     try {
         if(((typeof x) !== "number") || (typeof y !== "number")){
-            throw "x or y is not a number"
-        } else return x + y;
+            throw "Returns false if either argument is not of the data type number"
+        } else return x + y  
     }
     catch(err) {
         console.log(err)
-        return "broken"
+        return false
     }
 }
+
 // all numbers
 console.log(`${sum(1, 2)}`)
 // not all numbers
@@ -18,30 +19,53 @@ console.log(sum("1",2))
 console.log(sum(2, "2"))
 console.log(sum("1", "2"))
 
-
-
-console.log("\nProblem 2")
-// Given a user object, write a function called login that takes a username and password as parameters. Throw an error if either of them don't match. Otherwise, log to the console a message saying "login successful!"
-var user = {username: "sam", password: "123abc"};
-const login = (username, password) => (username === user.username) && (password === user.password) ? true : false;
-
-const login1 = login("sam", "123abc")
-const login2 = login("notsam", "123abc")
-
-
+// 1b) Call the sum function inside a try block using "1" and "2" as arguments. Use console.log within a catch block to inform the user of the error.
+console.log("\nquestion 1b\n")
 try {
-    if(login1){
-        console.log("login successful!")
-    } else throw "Check Credentials!"
+    let sumCheck = sum("1", "2")
+    if(sumCheck === false) throw 'Error becuase the stings "1" and "2" are not numbers'
+}
+catch(err) {
+    console.log(err)
+}
+
+// 2a) Given a user object, write a function called login that takes a username and password as parameters. Throw an error if either of them don't match. Otherwise, log to the console a message saying "login successful!"
+console.log("\nquestion 2a\n")
+var user = {username: "sam", password: "123abc"};
+function login(username, password){
+    const loginAttempt = (username === user.username) && (password === user.password) ? true : false;
+    try {
+        if(!loginAttempt) {
+            throw "Error check credentials"
+    }   else console.log("login successful!")
+    }
+    catch(err) {
+        console.log(err)
+    }
+    return loginAttempt
+}
+// Correct Login
+console.log(login("sam", "123abc"))
+// Incorrect Login
+console.log(login("notsam", "456def"))
+
+// 2b) Call the login function within a try block. In one instance use the correct credentials, and in another use incorrect ones. Make sure you see the appropriate message!
+console.log("\nquestion 2b\n")
+const correctCredentials = login("sam", "123abc")
+const incorrectCredentials = login("notsam", "456def")
+try {
+    if(correctCredentials) {
+        console.log("No error")
+    } else throw "Big Error"
 }
 catch(err) {
     console.log(err)
 }
 
 try {
-    if(login2){
-        console.log("login successful!")
-    } else throw "Check Credentials!"
+    if(incorrectCredentials) {
+        console.log("No error")
+    } else throw "Big Error"
 }
 catch(err) {
     console.log(err)
