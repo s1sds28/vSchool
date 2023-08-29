ad03024e3d165c1b6c4f55ee25e8cfb668411504
Steven Sparks <s1sds28@gmail.com>
2023-08-29 14:59:47 -0400

    Remove unneccessary .flat() method

--- level_1/exercise/sp51.5_forception/index.js
+++ level_1/exercise/sp51.5_forception/index.js
@@ -4,6 +4,7 @@ Output:
 ["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 */
 
+// output = [].flat()
 function forception(people, alphabet){
     let result = []
     for(let i = 0; i < people.length; i++){
@@ -12,7 +13,7 @@ function forception(people, alphabet){
             result.push(alphabet[i].toUpperCase()) 
         }
     }
-    return result.flat()
+    return result
 }
 
 var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
