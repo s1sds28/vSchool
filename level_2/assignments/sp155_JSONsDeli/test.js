0c942857140001bd31fd05a701e78ce348081a91
Steven Sparks <s1sds28@gmail.com>
2023-09-01 21:22:27 -0400

    Complete sp155 JSONs Deli

---
+++ level_2/assignments/sp155_JSONsDeli/test.js
@@ -0,0 +1,56 @@
+const jsonData = {
+    "menu": {
+      "appetizers": [
+        {
+          "name": "pretzels",
+          "ingredents": ["1", "2", "3"],
+          "cost": 0,
+          "description": "Salty"
+        }
+      ],
+      "coldSandwiches": [
+        {
+          "name": "ham",
+          "ingredents": ["1", "2", "3"],
+          "cost": 0,
+          "description": "Salty"
+        }
+      ],
+      "hotSandwiches": [
+        {
+          "name": "tunaMelt",
+          "ingredents": ["1", "2", "3"],
+          "cost": 0,
+          "description": "Salty"
+        }
+      ],
+      "desserts": [
+        {
+          "name": "ice cream",
+          "ingredents": ["1", "2", "3"],
+          "cost": 0,
+          "description": "Salty"
+        }
+      ],
+      "drinks": [
+        {
+          "name": "coffee",
+          "ingredents": ["1", "2", "3"],
+          "cost": 0,
+          "description": "Hot"
+        },
+        {
+          "name": "tea",
+          "ingredents": ["1", "2", "3"],
+          "cost": 0,
+          "description": "Hot"
+        }
+      ]
+    }
+}
+
+// console.log(jsonData.menu)
+
+// console.log(jsonData.menu.appetizers)
+
+console.log(jsonData.menu.drinks)
