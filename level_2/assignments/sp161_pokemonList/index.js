7c6c4ccfdaff6815fca3faea27142c5efd19b39a
Steven Sparks <s1sds28@gmail.com>
2023-09-03 14:44:15 -0400

    Complete sp161 Pokemon List

---
+++ level_2/assignments/sp161_pokemonList/index.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("button")
+
+const xhr = new XMLHttpRequest()
+
+button.addEventListener("click", () => {
+
+    xhr.open("GET", "https://api.vschool.io/pokemon", true)
+    xhr.send()
+    // xhr.onreadystatechange = () => {
+    //     if(xhr.readyState === 4 && xhr.status === 200){
+    //         const JSONdata = xhr.responseText
+    //         const data = JSON.parse(JSONdata)
+    //         const arrPokemon = data.objects[0].pokemon
+    //         arrPokemon.map((pokemon) => {
+    //             const h1 = document.createElement("h1")
+    //             h1.textContent = pokemon.name
+    //             document.body.appendChild(h1)
+    //         })
+    //     } 
+    // }
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4 && xhr.status === 200){
+            JSON.parse(xhr.responseText).objects[0].pokemon.map((pokemon) => {
+                const h1 = document.createElement("h1")
+                h1.textContent = pokemon.name
+                document.body.appendChild(h1)
+            })
+        } 
+    }
+})
