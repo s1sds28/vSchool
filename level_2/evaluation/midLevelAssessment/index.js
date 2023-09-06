27b9e65a525206d6bb83a762cbab5fbe2deec059
Steven Sparks <s1sds28@gmail.com>
2023-09-06 12:06:11 -0400

    Complete Mid Level 2 Review

--- level_2/evaluation/midLevelAssessment/index.js
+++ level_2/evaluation/midLevelAssessment/index.js
@@ -4,18 +4,21 @@ function sortNumbers(numbers) {
 }
 // const sortNumbers = numbers => numbers.sort()
 // Example usage:
-const numbers = [4, 2, 7, 1, 9, 5];
+// const numbers = [4, 2, 7, 1, 9, 5];
+// const numbers = ["1", "3", "2"]
+const numbers = ["one", "two", "three"]
+
 const sortedNumbers = sortNumbers(numbers);
 console.log(sortedNumbers);  // Output: [1, 2, 4, 5, 7, 9]
 
 // Write a function that takes an array of strings and returns a new array where each string is converted to uppercase.
-// function convertToUppercase(strings) {
-//     return strings.map(function(string) {
-//         return string.toUpperCase()
-//     })
-// }
+function convertToUppercase(strings) {
+    return strings.map(function(string) {
+        return string.toUpperCase()
+    })
+}
 
-const convertToUppercase = strings => strings.map(string => string[0].toUpperCase() + string.slice(1))
+// const convertToUppercase = strings => strings.map(string => string[0].toUpperCase() + string.slice(1))
 // Example usage:
 const strings = ['hello', 'world', 'javascript'];
 const uppercaseStrings = convertToUppercase(strings);
