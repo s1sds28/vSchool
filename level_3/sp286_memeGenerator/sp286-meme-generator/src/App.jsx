c2657c5236f4cf8f2bfbd6040fb23c2f71ade770
Steven Sparks <s1sds28@gmail.com>
2023-10-12 18:53:11 -0400

    Add Save button

--- level_3/sp286_memeGenerator/sp286-meme-generator/src/App.jsx
+++ level_3/sp286_memeGenerator/sp286-meme-generator/src/App.jsx
@@ -76,6 +76,7 @@ function App() {
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
+            <button className="save--button" onClick={saveMeme}>Save meme</button>
         </main>
     </>
   )
