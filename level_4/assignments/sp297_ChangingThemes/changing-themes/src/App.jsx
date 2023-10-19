8c318e69105143f43d4988f227bd771852d9f3eb
Steven Sparks <s1sds28@gmail.com>
2023-10-19 09:04:04 -0400

    Update components and css

--- level_4/assignments/sp297_ChangingThemes/changing-themes/src/App.jsx
+++ level_4/assignments/sp297_ChangingThemes/changing-themes/src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Footer from './components/Footer'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+
+
 function App() {
 
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='app-container'>
+        <Header></Header>
+        <MainContent></MainContent>
+        <Footer></Footer>
+      </div>
     </>
   )
 }
---
+++ level_4/assignments/sp297_ChangingThemes/changing-themes/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../App.css'
+
+function Footer(props){
+
+    return (
+        <footer className='footer'>My Awesome Footer</footer>
+    )
+}
+
+export default Footer;
