0a606137fa688d8777ec087ef0c696dc2baeb81e
Steven Sparks <s1sds28@gmail.com>
2023-10-22 16:27:04 -0400

    Add state for UglyThing

--- level_4/assignments/sp313_uglyThings/ugly-things/src/components/UglyThing.jsx
+++ level_4/assignments/sp313_uglyThings/ugly-things/src/components/UglyThing.jsx
@@ -1,1 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { v4 as uuidv4 } from 'uuid';
+
+import '../index.css'
+
+function UglyThing(props){
+
+    const { db_id, title, description } = props
+
+    const [toggle, setToggle] = useState(true)
+
+    // set State for uglyThing: 
+    const [uglyThing, setUglyThing] = useState({
+        id: uuidv4(),
+        db_id: db_id,
+        title: title,
+        description: description,
+    })
+
+    function handleChange(e){
+        e.preventDefault()
+
+        const { name, value } = e.target
+
+        setUglyThing(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log("handle submit")
+
+        
+
+
+
+        setToggle(prev => !prev)
+    }
+
+    function handleSave(e){
+        e.preventDefault()
+        console.log("Save")
+    }
+
+    function handleDelete(e){
+        e.preventDefault()
+        console.log("Delete")
+    }
+
+    function handleEdit(e){
+        e.preventDefault()
+        console.log("Edit")
+    }
+
+    
+
+    return (
+        <>{toggle ? (
+            <div key={uglyThing.id}>
+                {uglyThing.id}
+                {uglyThing.title}
+                {uglyThing.description}
+                {uglyThing.db_id}
+                <br/>
+                <button className="button" onClick={() => setToggle(prevState => !prevState)}>Edit</button>
+                <button className="button" onClick={handleDelete}>Delete</button>
+            </div>
+            ) : (
+            <form onSubmit={handleSubmit}>
+                <input
+                value={uglyThing.title}
+                name="title"
+                onChange={handleChange}
+                placeholder={uglyThing.title}
+                />
+     
+            <button type="submit" className="button">Save</button>
+            <button className="button" onClick={() => setToggle(prevState => !prevState)}>Cancel</button>        
+            </form>
+            )}
+        </>
+    )
+}
+
+export default UglyThing;
--- level_4/assignments/sp313_uglyThings/ugly-things/src/components/UglyThings.jsx
+++ level_4/assignments/sp313_uglyThings/ugly-things/src/components/UglyThings.jsx
@@ -1,27 +1,17 @@
 import React, {useState, useContext, useEffect} from 'react'
 
+
+import UglyThing from './UglyThing'
+
 import axios from 'axios'
+
+import {v4 as uuidv4} from 'uuid'
 
 function UglyThings(props){
 
     const [uglyData, setUglyData] = useState([])
-
-
-    function handleSave(props){
-        console.log(Save)
-    }
-
-    function handleDelete(props){
-        console.log(Delete)
-    }
-
-    function handleEdit(props){
-        console.log(Edit)
-    }
-
     
     const url = "https://api.vschool.io/steven/thing"
-
 
     useEffect(()=>{
         axios.get(url)
@@ -31,10 +21,25 @@ function UglyThings(props){
 
     console.log('array', uglyData)
 
+    const allUglyThings = uglyData.map((uglyThing) => {
+
+
+
+        return (
+            <UglyThing
+            key={uuidv4()}
+            db_id={uglyThing._id}
+            title={uglyThing.title}
+            description={uglyThing.description}
+            >
+
+            </UglyThing>
+        )
+    })
 
     return(
         <>
-            TEXT
+            { allUglyThings }
         </>
     )
 }
