a8ebc75bb9f7c9d43e417163199390ab60e47653
Steven Sparks <s1sds28@gmail.com>
2023-10-30 16:53:40 -0400

    Update about page

--- level_4/level4_capstone/stock-review-app/src/components/AboutPage.jsx
+++ level_4/level4_capstone/stock-review-app/src/components/AboutPage.jsx
@@ -5,54 +5,40 @@ import './components.css';
 import { Link } from 'react-router-dom';
 
 const AboutPage = () => {
+
+
   return (
     <div className="about-page">
       <h1>About Stock Research</h1>
       <p>The Stock Research application retrieves the highest and lowest trading prices for a stock. Below, I've outlined a case for your reference</p>
-      <h1>Utilize the Research page to analyze a stock of your choice</h1>
-      <h3>Example stock: AAPL, MSFT</h3>
+ 
       <section className='example'>
 
       <div className='input-container'>
-        <input placeholder="AAPL"/>
+        <input value="AAPL"/>
         <p className='about--description'>Enter the Ticker</p>
       </div>
       <div className="arrow-container">
           <div className="arrow"></div>
           <div className="text">Next Step</div>
       </div>
-      <div className='card'>
-        <h2>Save the data</h2>
-          <p>{`Date: XXX`}</p>
-          <p>{`Ticker: XXX`}</p>
-          <p>{`dayHigh: XXX`}</p>
-          <p>{`dayLow: XXX`}</p>
-        </div>
-
+        <input
+        type='date'
+        value="2023-01-01"/>
+        <p style={{margin: "10px"}}>Enter the Date</p>
 
         <div className="arrow-container">
           <div className="arrow"></div>
           <div className="text">Next Step</div>
         </div>
-        <div className='card'>
-        <h2>Saved for review</h2>
-          <p>{`Date: XXX`}</p>
-          <p>{`Ticker: XXX`}</p>
-          <p>{`dayHigh: XXX`}</p>
-          <p>{`dayLow: XXX`}</p>
-        </div>
-        <div className='card'>
-        <h2>Saved for review</h2>
-          <p>{`Date: XXX`}</p>
-          <p>{`Ticker: XXX`}</p>
-          <p>{`dayHigh: XXX`}</p>
-          <p>{`dayLow: XXX`}</p>
-        </div>
+          <button style={{margin: "10px"}}>Fetch Data</button>
         <div className="arrow-container">
           <div className="arrow"></div>
           <div className="text">Next Step</div>
-      </div>
-        <h3>Go to the Research Page</h3>
+      </div>      
+      <Link to="/research">
+        <button>Try it yourself :)</button>
+      </Link>
       </section>
     </div>
   );
