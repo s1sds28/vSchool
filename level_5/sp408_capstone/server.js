156a50316304d2a1bae5eddc81f7456532e6ac5a
Steven Sparks <s1sds28@gmail.com>
2023-11-20 18:16:54 -0500

    Update server.js

--- level_5/sp408_capstone/server.js
+++ level_5/sp408_capstone/server.js
@@ -8,27 +8,16 @@ const cors = require('cors')
 app.use(express.json())
 app.use(morgan('dev'))
 
+// middleware to allow cross-origin requests
+app.use(cors())
 
 // DB connect
 mongoose.connect("mongodb+srv://s1sds28:FHcQQhMkoKBKdT9j@cluster0.rblfy6t.mongodb.net/",
     () => console.log("Connected to DB"))
 
-
-app.use(cors())
 // Routes
-
-
 app.use("/bills", require("./routes/billRouter.js"))
 app.use("/billProvider", require("./routes/billProviderRouter.js"))
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// });
-
-
 
 // error handler
 app.use((err, req, res, next) => {
