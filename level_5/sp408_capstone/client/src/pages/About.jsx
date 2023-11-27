acda662a2c5402ead534320c9749611462284c73
Steven Sparks <s1sds28@gmail.com>
2023-11-27 13:30:57 -0500

    level 5 review

--- level_5/sp408_capstone/client/src/pages/About.jsx
+++ level_5/sp408_capstone/client/src/pages/About.jsx
@@ -5,8 +5,8 @@ export default function About() {
     return (
         <div style={{ padding: 20 }}> 
           <h1>To use the app, Go to the track bills page</h1>
-          <h2>Then add an account that has bills. For example, a Car account.</h2>
-          <h3>Lastly, add the bills you want to track. A Car account might have a January, February, and March bill</h3>
+          <h2>Then add a bill provider that has bills. For example, a Car bill provider.</h2>
+          <h3>Lastly, add the bills you want to track. A Car bill provider might have a January, February, and March bill</h3>
         </div>
       );
-    }      
+    }
--- level_5/sp408_capstone/client/src/pages/Home.jsx
+++ level_5/sp408_capstone/client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   
   return (
       <div style={{ padding: 20 }}> 
-          <h1>This app helps us keep track of Accounts and Bills</h1>
+          <h1>This app helps us keep track of Bill Providers and Bills</h1>
       </div>
     );
 }
