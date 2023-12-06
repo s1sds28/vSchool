3971c220a776b7f060425a51b2d85d20a18159f3
Steven Sparks <s1sds28@gmail.com>
2023-12-06 15:01:23 -0500

    Add routes for issues

---
+++ level_6/sp423_RTV/routes/issueRouter.jsx
@@ -0,0 +1,70 @@
+const express = require("express");
+const issueRouter = express.Router();
+const Issue = require('../models/Issue.jsx');
+
+// GET issues by user id
+issueRouter.get("/user", (req, res, next) => {
+  Issue.find( {user: req.auth._id }, (err, issues) => {
+    if(err){
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(issues)
+  })
+})
+
+// GET all issues
+issueRouter.get("/", (req, res, next) => {
+  Issue.find((err, issues) => {
+    if (err) {
+      res.status(500);
+      return next(err);
+    }
+    return res.status(200).send(issues);
+  });
+});
+
+// POST a new issue
+issueRouter.post("/", (req, res, next) => {
+  req.body.user = req.auth._id;
+  const newIssue = new Issue(req.body);
+  newIssue.save((err, savedIssue) => {
+    if (err) {
+      res.status(500);
+      return next(err);
+    }
+    return res.status(201).send(savedIssue);
+  });
+});
+
+// Delete issue
+issueRouter.delete("/:issueId", (req, res, next) => {
+  Issue.findOneAndDelete(
+    { _id: req.params.issueId, user: req.auth._id },
+    (err, deletedIssue) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(200).send(`Successfully delete todo: ${deletedIssue.title}`)
+    }
+  )
+})
+
+// Update issue
+issueRouter.put("/:issueId", (req, res, next) => {
+  Issue.findOneAndUpdate(
+    { _id: req.params.issueId, user: req.auth._id },
+    req.body,
+    { new: true },
+    (err, updatedIssue) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(updatedIssue)
+    }
+  )
+})
+
+module.exports = issueRouter;
--- level_6/sp423_RTV/server.js
+++ level_6/sp423_RTV/server.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const {expressjwt} = require('express-jwt')
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { expressjwt } = require('express-jwt');
 
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(morgan('dev'));
 
 mongoose.connect(
   'mongodb+srv://s1sds28:M16Xi7AwoG129ZU9@cluster0.um1fant.mongodb.net/',
   () => console.log('Connected to the DB')
-)
+);
 
-app.use('/auth', require('./routes/authRouter.jsx'))
-app.use('/api', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] })) // req.user
-// app.use('/api/todo', require('./routes/todoRouter.js'))
+// Import and apply 
+const issueRouter = require('./routes/issueRouter.jsx');
+app.use('/auth', require('./routes/authRouter.jsx'));
+app.use('/api', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] })); // req.user
+app.use('/api/issue', issueRouter);
 
 app.use((err, req, res, next) => {
-  console.log(err)
-  if(err.name === "UnauthorizedError"){
-    res.status(err.status)
+  console.log(err);
+  if (err.name === 'UnauthorizedError') {
+    res.status(err.status);
   }
-  return res.send({errMsg: err.message})
-})
+  return res.send({ errMsg: err.message });
+});
 
 app.listen(9000, () => {
-  console.log(`Server is running on local port 9000`)
-})
+  console.log(`Server is running on local port 9000`);
+});
