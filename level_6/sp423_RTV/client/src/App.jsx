dc5c0d29009f6082578a96ebffbfc64a59d6c631
Steven Sparks <s1sds28@gmail.com>
2023-12-20 10:23:03 -0500

    Update App

--- level_6/sp423_RTV/client/src/App.jsx
+++ level_6/sp423_RTV/client/src/App.jsx
@@ -1,44 +1,50 @@
-import React, { useContext, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx'
-import Auth from './components/Auth.jsx'
-import Profile from './components/Profile.jsx'
-import Public from './components/Public.jsx'
-import ProtectedRoute from './components/ProtectedRoute.jsx';
-import { UserContext } from './context/UserProvider.jsx'
+import React, { useContext, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 
-export default function App(){
-  const { token, logout, getAllComments } = useContext(UserContext)
-  useEffect(()=>{
-    console.log("app useEffect get all comments")
+import Navbar from './components/Navbar.jsx';
+import Auth from './components/Auth.jsx';
+import Profile from './components/Profile.jsx';
+import Public from './components/Public.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import { UserContext } from './context/UserProvider.jsx';
+
+export default function App() {
+  const { token, logout, getAllComments } = useContext(UserContext);
+
+  useEffect(() => {
+    console.log("app useEffect get all comments");
     getAllComments();
-  }, [])
+  }, []);
+
   return (
     <Router>
-    <div className="app">
-      { token && <Navbar logout={logout}/>}
-
-
-      <Routes>
-        <Route path="/" element={ token ? <Navigate to="/profile"/> : <Auth />} />
-        <Route 
-          path="/profile" 
-          element={
-            <ProtectedRoute token={token} redirectTo="/">
-              <Profile />
-            </ProtectedRoute>
-          } 
-        />
-        <Route
-        path="/public" 
-        element={
-          <ProtectedRoute token={ token } redirectTo="/">
-            <Public/>
-          </ProtectedRoute>
-        } 
-      />
-      </Routes>
-    </div>
+      <div className="app">
+        {token && <Navbar logout={logout} />}
+        <Routes>
+          <Route path="/" element={token ? <Navigate to="/profile" /> : <Auth />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute token={token} redirectTo="/">
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/public"
+            element={
+              <ProtectedRoute token={token} redirectTo="/">
+                <Public />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
