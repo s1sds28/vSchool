1c31a42ec4f066d2dcecb81147d0ecb68171116f
Steven Sparks <s1sds28@gmail.com>
2024-01-02 19:13:24 -0500

    WIP edit

--- level_6/sp423_RTV/client/src/components/Issue.jsx
+++ level_6/sp423_RTV/client/src/components/Issue.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../context/UserProvider";
 
 import "../css/issues.css"
@@ -7,6 +7,33 @@ import CommentList from "./CommentList";
 export default function Issue(props) {
   const { comments, upVoteIssue, downVoteIssue } = useContext(UserContext);
   const { _id, likedUsers, dislikedUsers } = props;
+
+  const [isEditing, setIsEditing] = useState(false)
+  const [formData, setFormData] = useState({
+    title: [props.title],
+    description: [props.description]
+  })
+
+  function handleEditClick(e){
+    e.preventDefault()
+    setIsEditing(prev => !prev)
+  }
+
+  function handleInputChange(e){
+    const {name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setFormData({
+
+    })
+    setIsEditing(false)
+  }
 
   const filteredComments = comments.filter(comment => comment.issue === _id);
 
@@ -23,26 +50,44 @@ export default function Issue(props) {
 
   return (
     <>
-      <form className="issue">
-        <h1>{props.title}</h1>
-        <h3>{props.description}</h3>
-        
+    {isEditing ? (
+      <form className="issue" onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleInputChange}
+        />
+      </label>
+      <h3>{props.description}</h3>
+      <button onClick={handleEditClick}>Edit</button>
 
-        <div className="vote-container">
-          <div>
-            <button onClick={(e) => handleUpVote(e, _id)} className="up-vote">Up Vote</button>
-            <span>Upvotes: {likedUsers.length}</span>
-          </div>
-          <div>
-            <button onClick={(e) => handleDownVote(e, _id)} className="down-vote">DownVote</button>
-            <span>Downvotes: {dislikedUsers.length}</span>
-          </div>
-        </div>
-      
-        <div className="comments-container">
-          <CommentList filteredComments={ filteredComments } issue_Id={_id}/>
-        </div>
-      </form>
+
+    </form>
+    ):(
+    <form className="issue">
+    <h1>{props.title}</h1>
+    <h3>{props.description}</h3>
+    <button onClick={handleEditClick}>Edit</button>
+
+    <div className="vote-container">
+      <div>
+        <button onClick={(e) => handleUpVote(e, _id)} className="up-vote">Up Vote</button>
+        <span>Upvotes: {likedUsers.length}</span>
+      </div>
+      <div>
+        <button onClick={(e) => handleDownVote(e, _id)} className="down-vote">DownVote</button>
+        <span>Downvotes: {dislikedUsers.length}</span>
+      </div>
+    </div>
+  
+    <div className="comments-container">
+      <CommentList filteredComments={ filteredComments } issue_Id={_id}/>
+    </div>
+    </form>
+    )}
     </>
   );
 }
