6d98d4700b9dad661dfe4469f2d23bf9780a24f6
Steven Sparks <s1sds28@gmail.com>
2024-01-03 15:08:11 -0500

    Clean comments

--- level_6/sp423_RTV/client/src/components/IssueList.jsx
+++ level_6/sp423_RTV/client/src/components/IssueList.jsx
@@ -1,16 +1,3 @@
-// import React from "react";
-// import Issue from "./Issue.jsx"
-
-// export default function IssueList(props){
-//     const { issues, isProfile } = props
-    
-//     return (
-//         <div className="issue-list">
-//             { issues.map(issue => <Issue {...issue} isProfile={ isProfile } key={issue._id}/>) }
-//         </div>
-//     )
-// }
-
 import React from "react";
 import Issue from "./Issue.jsx";
 
