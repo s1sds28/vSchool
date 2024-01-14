1bbb48d94b2d321f827fcfa376efb30c8c860060
Steven Sparks <s1sds28@gmail.com>
2024-01-14 16:47:54 -0500

    Add graph

--- level_6/sp470_budget/client/src/components/Home.jsx
+++ level_6/sp470_budget/client/src/components/Home.jsx
@@ -1,22 +1,47 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+
+import BarChart from './BarChart.jsx'
+
+import { UserData } from './Data.jsx'
 
 import { UserContext } from '../context/UserProvider.jsx'
 
 export default function Home(){
-  const { 
-    user: { 
-      username,
-      accounts,
-      bills
-    },
 
-  } = useContext(UserContext)
+  const {accounts, getUserData} = useContext(UserContext)
+
+  useEffect(() => {
+    getUserData();
+  }, []);
 
 
+  
+  const [userData, setUserData] = useState({
+    labels:[
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+    datasets: [
+      {
+        label: "Monthly Budget",
+        data: [5,5,5,5,5,5,5,5,5,5,5,5],
 
-  return (
-    <>
-      <p>Home</p>
-    </>
-  )
+      },
+    ],
+  })
+  return <div className="App">
+    <div className="">
+    <BarChart chartData={userData}/>
+    </div>
+  </div>
 }
