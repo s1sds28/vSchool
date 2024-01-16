97c1cbd35f35b0c5ffecebc63157c6f5264e7724
Steven Sparks <s1sds28@gmail.com>
2024-01-15 19:04:18 -0500

    Update chart

--- level_6/sp470_budget/client/src/components/BarChart.jsx
+++ level_6/sp470_budget/client/src/components/BarChart.jsx
@@ -6,47 +6,9 @@ import { UserContext } from '../context/UserProvider.jsx'
 import { Bar } from 'react-chartjs-2'
 import {Chart as Chartjs} from 'chart.js/auto'
 
+import '../css/barChart.css'
+
 function BarChart({chartData}) {
-
-
-  const {accounts, getUserData} = useContext(UserContext)
-
-  useEffect(() => {
-    getUserData();
-  }, []);
-
-  const costPerMonth = accounts.reduce((sum, account) => {
-    return sum += account.budgetAmount
-  }, 0)
-
-  const eachMonth = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ]
-
-  const [userData, setUserData] = useState({
-    labels: eachMonth,
-    datasets: [
-      {
-        label: "Costs per Month",
-        data: eachMonth.map(()=> costPerMonth/12),
-
-      },
-    ],
-  })
-
-
-
-  return <Bar data={userData} />
+  return <Bar data={chartData} />
 }
 export default BarChart
--- level_6/sp470_budget/client/src/components/Home.jsx
+++ level_6/sp470_budget/client/src/components/Home.jsx
@@ -1,13 +1,60 @@
-import React from 'react'
+import React, {useContext, useState, useEffect} from 'react'
+
+import { UserContext } from '../context/UserProvider.jsx'
+
 
 import BarChart from './BarChart.jsx'
 
+import {Chart as Chartjs} from 'chart.js/auto'
+
+
 export default function Home(){
 
-  return <>
 
+  const {accounts, getUserData} = useContext(UserContext)
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const costPerMonth = accounts.reduce((sum, account) => {
+    return sum += account.budgetAmount
+  }, 0)
+
+  const eachMonth = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+
+  const [userData, setUserData] = useState({
+    labels: eachMonth,
+    datasets: [
+      {
+        label: "Costs per Month",
+        data: eachMonth.map(()=> costPerMonth/12),
+
+      },
+    ],
+  })
+
+
+
+  return <>
     <div className="monthly-chart-container">
-      <BarChart/>
+      <BarChart chartData={userData}/>
+    </div>
+    <div style={{ justifyContent: 'center', textAlign: 'center', marginTop: '20px' }}>
+      To be continued... a report: A years breakdown of when bill are due similar to a cash flow statement
     </div>
   </>
 }
